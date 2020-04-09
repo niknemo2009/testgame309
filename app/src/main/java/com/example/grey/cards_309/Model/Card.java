e65a1d28538ebd35f6cd656434339bc948c2999c
Nezdoliy <nezdoliy.yura@gmail.com>
2020-04-09 11:42:10 +0300

    first commit

---
+++ app/src/main/java/com/example/grey/cards_309/Model/Card.java
@@ -0,0 +1,8 @@
+package com.example.grey.cards_309.Model;
+
+public class Card {
+}
+
+enum Mast{
+    PICA,TREFA,BUBNA,CHERVI
+}
---
+++ app/src/main/java/com/example/grey/cards_309/Model/Game.java
@@ -0,0 +1,4 @@
+package com.example.grey.cards_309.Model;
+
+public class Game {
+}
